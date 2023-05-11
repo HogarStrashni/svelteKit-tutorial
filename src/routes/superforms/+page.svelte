be562fe15c41afa35b71fb8609ab866bf0aca36d
Đorđe Matić <djmatic@agfbl.org>
2023-05-11 23:17:02 +0200

    remove types

--- src/routes/superforms/+page.svelte
+++ src/routes/superforms/+page.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-	import type { PageData } from './$types';
 	import { superForm } from 'sveltekit-superforms/client';
 	// import SuperDebug from 'sveltekit-superforms/client/SuperDebug.svelte';
 
-	export let data: PageData;
+	export let data;
 
 	const { form, errors, enhance, constraints } = superForm(data.form);
 </script>
